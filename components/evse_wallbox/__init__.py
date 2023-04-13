77fe08b2b479c42969377a4af217115ce74a4197
Sebastian Muszynski <basti@linkt.de>
2023-04-13 09:54:03 +0200

    Add old firmware support (#13)

--- components/evse_wallbox/__init__.py
+++ components/evse_wallbox/__init__.py
@@ -10,6 +10,7 @@ AUTO_LOAD = ["binary_sensor", "number", "sensor", "switch", "text_sensor"]
 MULTI_CONF = True
 
 CONF_EVSE_WALLBOX_ID = "evse_wallbox_id"
+CONF_FIRMWARE_VERSION = "firmware_version"
 
 evse_wallbox_ns = cg.esphome_ns.namespace("evse_wallbox")
 EvseWallbox = evse_wallbox_ns.class_(
@@ -26,6 +27,7 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(EvseWallbox),
+            cv.Optional(CONF_FIRMWARE_VERSION, default=18): cv.positive_int,
         }
     )
     .extend(cv.polling_component_schema("5s"))
@@ -37,3 +39,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await modbus.register_modbus_device(var, config)
+
+    cg.add(var.set_firmware_version(config[CONF_FIRMWARE_VERSION]))
