4795a22a04e27aa86542ad22bf880e9fff6c78ef
Sebastian Muszynski <basti@linkt.de>
2023-04-20 10:29:35 +0200

    Add ESPHome 2023.4.0 compatibility (#16)

--- components/evse_wallbox/number/evse_number.cpp
+++ components/evse_wallbox/number/evse_number.cpp
@@ -8,7 +8,7 @@ static const char *const TAG = "evse_wallbox.number";
 
 void EvseNumber::dump_config() { LOG_NUMBER("", "EvseWallbox Number", this); }
 void EvseNumber::control(float value) {
-  this->parent_->write_register(this->holding_register_, (uint16_t)(value * (1.0f / this->traits.get_step())));
+  this->parent_->write_register(this->holding_register_, (uint16_t) (value * (1.0f / this->traits.get_step())));
 }
 
 }  // namespace evse_wallbox
